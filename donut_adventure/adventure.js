0a2168404342482f6f9161ae08ae08c9cef2c4b1
lstocker327 <lukestocker1@gmail.com>
2021-05-13 16:42:48 -0400

    created our enemy pizza rat

--- donut_adventure/adventure.js
+++ donut_adventure/adventure.js
@@ -1,1 +1,53 @@
-console.log("hello world");
+class Hero{
+  constructor(name){
+    this.name = name;
+    this.health = 100;
+    this.weapons = {
+      sprinkleSpray: 5,
+      sugarShock: 10
+    }
+    this.catchPhrases = ['i\'m fresher than day old pizza',
+ 'you can\'t count my calories']
+  }
+
+  talkSass(){
+    console.log(this.catchPhrases[Math.floor(Math.random() * 2)])
+  }
+  announceHealth(){
+    console.log(this.health)
+  }
+  fight(){
+    ("im ready to rumble")
+  }
+}
+
+const dougieTheDonut = new Hero("Dougie")
+dougieTheDonut.talkSass();
+
+class Enemy{
+  constructor(name){
+    this.name = name;
+    this.health = 100;
+    this.weapons = {
+      pepperoniStars: 5,
+      cheeseGrease: 10
+    }
+    this.catchPhrases = [
+      'i\'m youtube famous',
+      'i\'m more dangerous than an uncovered sewer'
+    ]
+    }
+
+  talkSmack(){
+    console.log(this.catchPhrases[Math.floor(Math.random() * 2)])
+  }
+  announceHealth(){
+    console.log(this.health)
+  }
+  fight(){
+    console.log("im gonna flatten you like a slice of pepperoni")
+  }
+}
+
+const pizzaRat = new Enemy("Pizza Rat");
+pizzaRat.talkSmack();
